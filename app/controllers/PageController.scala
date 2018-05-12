4e17d7f3445c15e426bdd180a91bcf11b86df23b
kw7oe <choongkwern@hotmail.com>
2018-05-12 17:08:38 +0800

    Refactor flash message

--- app/controllers/PageController.scala
+++ app/controllers/PageController.scala
@@ -4,8 +4,7 @@ import javax.inject._
 import play.api.mvc._
 
 class PageController @Inject()(cc: ControllerComponents)
- extends AbstractController(cc) {
-
+ extends AbstractController(cc) with play.api.i18n.I18nSupport {
 
   /**
    * The index action.
