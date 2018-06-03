61e269517cbb6cf7b02e70799812d071f96e0b89
kw7oe <choongkwern@hotmail.com>
2018-06-03 17:19:28 +0800

    Add in sortable table to Attendance

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -4,5 +4,20 @@ $(document).ready(function() {
     trigger: 'hover',
     html: true
   })
+
+  $('#example').DataTable();
+  $('.dataTables_wrapper').find('label').each(function() {
+    $(this).parent().append($(this).children());
+  });
+  $('.dataTables_filter').find('input').each(function() {
+    $('input').attr("placeholder", "Search");
+    $('input').removeClass('form-control-sm');
+  });
+  $('.dataTables_length').addClass('d-flex flex-row');
+  $('.dataTables_filter').addClass('md-form');
+  $('select').addClass('mdb-select');
+  $('.mdb-select').material_select();
+  $('.mdb-select').removeClass('form-control form-control-sm');
+  $('.dataTables_filter').find('label').remove();
 })
 
