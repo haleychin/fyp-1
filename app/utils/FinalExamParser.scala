b71f0566d9e85bc9e1c0be3299b0a554982d44ae
kw7oe <choongkwern@hotmail.com>
2018-06-09 11:53:57 +0800

    WIP: Now can save exam result to db

--- app/utils/FinalExamParser.scala
+++ app/utils/FinalExamParser.scala
@@ -50,17 +50,61 @@ class FinalExamParser {
     val formatter = new DataFormatter()
     val sheet     = workbook.getSheetAt(0)
 
-    val examInfoRow    = sheet.getRow(1)
-    // val total          = examInfoRow.getCell(2).getNumericCellValue()
-    // val totalWeightage = examInfoRow.getCell(3).getNumericCellValue()
-
+    // =================
     // Save Metric to DB
+    // =================
     val metricSheet = workbook.getSheetAt(1)
     for (row <- metricSheet) {
       val name         = formatter.formatCellValue(row.getCell(0))
       val description  = formatter.formatCellValue(row.getCell(1))
-
       mRepo.create(courseId, name, description)
+    }
+
+    // =================================================
+    // Get first question and last question column index
+    // =================================================
+    val firstRow = sheet.getRow(0)
+    var questionStart = 0
+    var questionEnd   = 0
+    for (cell <- firstRow) {
+      val value = cell.getStringCellValue
+      if (value == "Questions") {
+        questionStart = cell.getColumnIndex()
+      } else if (value == "Total") {
+        questionEnd   = cell.getColumnIndex() - 1
+      }
+    }
+
+    // =================================
+    // Get exam total mark and weightage
+    // =================================
+    val rowOne         = sheet.getRow(1)
+    val total          = rowOne.getCell(questionEnd + 1).getNumericCellValue()
+    val totalWeightage = rowOne.getCell(questionEnd + 2).getNumericCellValue()
+
+    // ====================
+    // Get question details
+    // ====================
+    var i: Int = 0;
+    for (i <- questionStart to questionEnd) {
+      val metricRow   = sheet.getRow(2)
+      val fullMarkRow = sheet.getRow(3)
+
+      // Question Info
+      val name        = rowOne.getCell(i).getStringCellValue()
+      val metric      = metricRow.getCell(i).getStringCellValue()
+      val fullMark    = fullMarkRow.getCell(i).getNumericCellValue()
+
+      for (row <- sheet) {
+        if (row.getRowNum > 3) {
+          val studentId = row.getCell(0).getStringCellValue()
+          val mark      = row.getCell(questionEnd + 1).getNumericCellValue()
+          val weightage = row.getCell(questionEnd + 2).getNumericCellValue()
+
+          // Create exam record
+          eRepo.create(courseId, studentId, mark, total, weightage, totalWeightage)
+        }
+      }
     }
 
   }
